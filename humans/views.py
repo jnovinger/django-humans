1359622a7ae59843b4e907b3be1c895ae651b595
Jason Novinger <jnovinger@gmail.com>
2012-08-16 19:10:07 -0500

    View to render up a humans.txt

--- humans/views.py
+++ humans/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+from humans.models import Snippet
+
+def humans(request):
+    snippets = Snippet.objects.all()
+
+    return render_to_response(
+        template_name='humans/humans.txt',
+        dictionary={'snippets': snippets},
+        mimetype='text/plain',
+        context_instance=RequestContext(request),
+    )
+
