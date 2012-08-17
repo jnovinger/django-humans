b215c082e3b27336f781aaa7469dc632af12ec82
Jason Novinger <jnovinger@gmail.com>
2012-08-16 19:59:46 -0500

    Add Humans HumanGroups to view/template.

--- humans/views.py
+++ humans/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from humans.models import Snippet
+from humans.models import HumanGroup, Snippet
 
 def humans(request):
     snippets = Snippet.objects.all()
+    groups = HumanGroup.objects.all()
 
     return render_to_response(
         template_name='humans/humans.txt',
-        dictionary={'snippets': snippets},
+        dictionary={
+            'snippets': snippets,
+            'groups': groups,
+        },
         mimetype='text/plain',
         context_instance=RequestContext(request),
     )
